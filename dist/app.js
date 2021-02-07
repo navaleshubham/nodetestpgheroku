d2735462b68c27c9a84a47967c7683fcdebe4252
navaleshubham <shubham.navale125@gmail.com>
2021-02-07 20:28:55 +0530

    final

--- dist/app.js
+++ dist/app.js
@@ -1,20 +1,29 @@
+//library
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from "cors";
+//class and functions
 import Pool from './db.js';
 import userrouter from "./routes/user.js";
-const app = express(); // initialize the express server
-// create a test route
+// initialize the express server
+const app = express();
+// database conection test
 Pool.connect(function (err, client, done) {
     if (err)
         console.log(err);
     console.log('Connected');
 });
-app.get('/', (req, res, next) => {
-    res.send('Hello world');
-});
+//configuration
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(bodyParser.json());
+app.use(cors());
+//routes
 app.use('/user', userrouter);
+//demo route
+// app.get('/', (req, res, next) => {
+//     res.send('Hello world')
+// })
 // Define the port to run the server. this could either be defined 
 app.listen(process.env.PORT || 4000, () => {
     console.log("server started on port", process.env.PORT || 4000);
