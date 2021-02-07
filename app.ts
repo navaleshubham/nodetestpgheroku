9a196701877af69d50b0a5428d36fa69adf8170b
navaleshubham <shubham.navale125@gmail.com>
2021-02-07 17:40:49 +0530

    updated the prettier and eslint

--- app.ts
+++ app.ts
@@ -7,15 +7,16 @@ const app = express(); // initialize the express server
 Pool.connect(function (err, client, done) {
     if (err) console.log(err)
     console.log('Connected');
-}); 
+});
 
-app.get('/', (req, res, next) => {   
-   res.send('Hello world')
+app.get('/', (req, res, next) => {
+    res.send('Hello world')
 })
-app.use(bodyParser.urlencoded({ extended:true }))
-app.use(bodyParser.json( ));
-app.use('/user',userrouter)
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use('/user', userrouter)
 
 // Define the port to run the server. this could either be defined 
-app.listen(process.env.PORT || 4000, () => {console.log("server started on port",process.env.PORT|| 4000);
+app.listen(process.env.PORT || 4000, () => {
+    console.log("server started on port", process.env.PORT || 4000);
 })
