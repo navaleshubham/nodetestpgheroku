283064494306d11ccf3ecf4e77028e153f66dc92
navaleshubham <shubham.navale125@gmail.com>
2021-02-15 12:01:42 +0530

    authorization middleware added

--- app.ts
+++ app.ts
@@ -7,72 +7,71 @@ import jwt from 'jsonwebtoken';
 import Pool from './db';
 import userrouter from './routes/user';
 import cookieParser from 'cookie-parser';
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
 
 // initialize the express server
 const app = express();
-dotenv.config()
+dotenv.config();
 // database conection test
 Pool.connect(function (err: any, _client: any, _done: any) {
-    if (err) console.log(err);
-    creteorchecktable();
-    console.log('Connected all okay');
+  if (err) console.log(err);
+  creteorchecktable();
+  console.log('Connected all okay');
 });
 
 async function creteorchecktable() {
-    try {
-        const client = await Pool.connect();
-        const sql = `CREATE TABLE EMPLOYEE
+  try {
+    const client = await Pool.connect();
+    const sql = `CREATE TABLE EMPLOYEE
                    (first_name CHAR(20) NOT NULL, 
                    last_name CHAR(20) NOT NULL,
                    title CHAR(20) NOT NULL,
                    email CHAR(50) NOT NULL,
                    password CHAR(1000) NOT NULL,
                    PRIMARY KEY(email))`;
-        const result = await client.query(sql);
-        console.log(result);
-    } catch (error) {
-        console.log('table already exists');
-    }
+    const result = await client.query(sql);
+    console.log(result);
+  } catch (error) {
+    console.log('table already exists');
+  }
 }
 //auth check middleware
 async function authentication(req: express.Request, res: express.Response, next) {
-    try {
-        const token = await req.headers.authorization
-        console.log(req.headers.authorization)
-        const result = await jwt.verify(token, process.env.JWT_SECRET);
-        const user = await checkuser(result);
-        if (user) {
-            next();
-        } else {
-            res.status(404).send('user not found please check credentials and try again');
-        }
-    } catch {
-        console.log('token not found');
-        if (req.path == '/user/new' || req.path == '/user/login') {
-            next()
-        }
-        else {
-            res.status(404).send('token not found please login and try again');
-        }
+  try {
+    const token = await req.headers.authorization;
+    console.log(req.headers.authorization);
+    const result = await jwt.verify(token, process.env.JWT_SECRET);
+    const user = await checkuser(result);
+    if (user) {
+      next();
+    } else {
+      res.status(404).send('user not found please check credentials and try again');
     }
+  } catch {
+    console.log('token not found');
+    if (req.path == '/user/new' || req.path == '/user/login') {
+      next();
+    } else {
+      res.status(404).send('token not found please login and try again');
+    }
+  }
 }
 
 async function checkuser(data) {
-    try {
-        const client = await Pool.connect();
-        const sql = `SELECT * FROM EMPLOYEE WHERE email=${data.email}`;
-        const { rows } = await client.query(sql);
-        const EMPLOYEE = rows;
-        client.release();
-        if (EMPLOYEE.length == 1) {
-            return true;
-        } else {
-            return false;
-        }
-    } catch (error) {
-        return error;
+  try {
+    const client = await Pool.connect();
+    const sql = `SELECT * FROM EMPLOYEE WHERE email=${data.email}`;
+    const { rows } = await client.query(sql);
+    const EMPLOYEE = rows;
+    client.release();
+    if (EMPLOYEE.length == 1) {
+      return true;
+    } else {
+      return false;
     }
+  } catch (error) {
+    return error;
+  }
 }
 //configuration
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -80,7 +79,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
 app.use(authentication);
-app.use(cookieParser())
+app.use(cookieParser());
 //routes
 app.use('/user', userrouter);
 
@@ -91,5 +90,5 @@ app.use('/user', userrouter);
 
 // Define the port to run the server. this could either be defined
 app.listen(process.env.PORT || 4000, () => {
-    console.log('server started on port', process.env.PORT || 4000);
+  console.log('server started on port', process.env.PORT || 4000);
 });
--- controllers/user.ts
+++ controllers/user.ts
@@ -1,100 +1,96 @@
 import express from 'express';
 import pool from '../db';
 import jwt from 'jsonwebtoken';
-import bcryptjs from 'bcryptjs'
+import bcryptjs from 'bcryptjs';
 class UserController {
-    public async get(req: express.Request, res: express.Response) {
-        try {
-            const client = await pool.connect();
-            const sql = 'SELECT * FROM EMPLOYEE';
-            const { rows } = await client.query(sql);
-            const EMPLOYEE = rows;
-            client.release();
-            res.status(200).send(EMPLOYEE);
-        } catch (error) {
-            res.status(400).send(error);
-        }
+  public async get(req: express.Request, res: express.Response) {
+    try {
+      const client = await pool.connect();
+      const sql = 'SELECT * FROM EMPLOYEE';
+      const { rows } = await client.query(sql);
+      const EMPLOYEE = rows;
+      client.release();
+      res.status(200).send(EMPLOYEE);
+    } catch (error) {
+      res.status(400).send(error);
     }
-    public async post(
-        req: {
-            body: {
-                last_name: String;
-                first_name: String;
-                title: String;
-                email: String;
-                password: string;
-            };
-        },
-        res: express.Response
-    ) {
-        try {
-            const client = await pool.connect();
-            const salt = await bcryptjs.genSalt(10);
-            var password = await bcryptjs.hash(req.body.password, salt);
-            req.body.password = password.replace('\/s', "")
-            const sql = `INSERT INTO EMPLOYEE (last_name, first_name, title, email, password) VALUES('${req.body.last_name}','${req.body.first_name}','${req.body.title}','${req.body.email}','${req.body.password}')`;
-            const { rowCount } = await client.query(sql);
-            if (rowCount == 1) {
-                const expiresIn = 60 * 60; // an hour
-                const secret = process.env.JWT_SECRET;
-                const token = { expiresIn: expiresIn, token: jwt.sign(req.body.email, secret) };
-                res.setHeader('authorization', token.token)
-                res
-                    .status(200)
-                    .send({ rowCount: rowCount, result: rowCount == 1 ? true : false, token: token });
-
-            }
-            client.release();
-        } catch (error) {
-            res.status(500).send(error);
-        }
+  }
+  public async post(
+    req: {
+      body: {
+        last_name: String;
+        first_name: String;
+        title: String;
+        email: String;
+        password: string;
+      };
+    },
+    res: express.Response
+  ) {
+    try {
+      const client = await pool.connect();
+      const salt = await bcryptjs.genSalt(10);
+      var password = await bcryptjs.hash(req.body.password, salt);
+      req.body.password = password.replace('/s', '');
+      const sql = `INSERT INTO EMPLOYEE (last_name, first_name, title, email, password) VALUES('${req.body.last_name}','${req.body.first_name}','${req.body.title}','${req.body.email}','${req.body.password}')`;
+      const { rowCount } = await client.query(sql);
+      if (rowCount == 1) {
+        const expiresIn = 60 * 60; // an hour
+        const secret = process.env.JWT_SECRET;
+        const token = { expiresIn: expiresIn, token: jwt.sign(req.body.email, secret) };
+        res.setHeader('authorization', token.token);
+        res
+          .status(200)
+          .send({ rowCount: rowCount, result: rowCount == 1 ? true : false, token: token });
+      }
+      client.release();
+    } catch (error) {
+      res.status(500).send(error);
     }
-    public async login(req: { body: { email: String; password: string } }, res: express.Response) {
-        try {
-            const client = await pool.connect();
-            const sql = `SELECT password FROM EMPLOYEE WHERE email='${req.body.email}'`;
-            const { rows } = await client.query(sql);
-            const result = bcryptjs.compare(req.body.password, rows[0].password)
-            client.release();
-            if (result) {
-                const expiresIn = 60 * 60; // an hour
-                const secret = process.env.JWT_SECRET;
-                const token = { expiresIn: expiresIn, token: jwt.sign(req.body.email, secret) };
-                res.setHeader('authorization', token.token)
-                res
-                    .status(200)
-                    .send(token);
-            }
-
-        } catch (error) {
-            res.status(404).send(error);
-        }
+  }
+  public async login(req: { body: { email: String; password: string } }, res: express.Response) {
+    try {
+      const client = await pool.connect();
+      const sql = `SELECT password FROM EMPLOYEE WHERE email='${req.body.email}'`;
+      const { rows } = await client.query(sql);
+      const result = bcryptjs.compare(req.body.password, rows[0].password);
+      client.release();
+      if (result) {
+        const expiresIn = 60 * 60; // an hour
+        const secret = process.env.JWT_SECRET;
+        const token = { expiresIn: expiresIn, token: jwt.sign(req.body.email, secret) };
+        res.setHeader('authorization', token.token);
+        res.status(200).send(token);
+      }
+    } catch (error) {
+      res.status(404).send(error);
     }
-    public async update(
-        req: { body: { first_name: String; last_name: String; title: String; email: String } },
-        res: express.Response
-    ) {
-        try {
-            const client = await pool.connect();
-            const sql = `UPDATE EMPLOYEE SET FIRST_NAME='${req.body.first_name}',LAST_NAME='${req.body.last_name}',TITLE='${req.body.title}' WHERE EMAIL='${req.body.email}'`;
-            const { rowCount } = await client.query(sql);
-            client.release();
-            res.status(200).send({ rowcount: rowCount, result: rowCount == 1 ? true : false });
-        } catch (error) {
-            res.status(500).send(error);
-        }
+  }
+  public async update(
+    req: { body: { first_name: String; last_name: String; title: String; email: String } },
+    res: express.Response
+  ) {
+    try {
+      const client = await pool.connect();
+      const sql = `UPDATE EMPLOYEE SET FIRST_NAME='${req.body.first_name}',LAST_NAME='${req.body.last_name}',TITLE='${req.body.title}' WHERE EMAIL='${req.body.email}'`;
+      const { rowCount } = await client.query(sql);
+      client.release();
+      res.status(200).send({ rowcount: rowCount, result: rowCount == 1 ? true : false });
+    } catch (error) {
+      res.status(500).send(error);
     }
-    public async delete(req: { params: { email: number } }, res: express.Response) {
-        try {
-            const client = await pool.connect();
-            const sql = `DELETE FROM EMPLOYEE WHERE email=${req.params.email}`;
-            const { rowCount } = await client.query(sql);
-            client.release();
-            res.send({ rowcount: rowCount, result: rowCount == 1 ? true : false });
-        } catch (error) {
-            res.status(400).send(error);
-        }
+  }
+  public async delete(req: { params: { email: number } }, res: express.Response) {
+    try {
+      const client = await pool.connect();
+      const sql = `DELETE FROM EMPLOYEE WHERE email=${req.params.email}`;
+      const { rowCount } = await client.query(sql);
+      client.release();
+      res.send({ rowcount: rowCount, result: rowCount == 1 ? true : false });
+    } catch (error) {
+      res.status(400).send(error);
     }
+  }
 }
 
 export default UserController;
